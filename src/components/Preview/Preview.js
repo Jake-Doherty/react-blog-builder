87425da337f5a59a498968eb65d2acc7a98c1ecc
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-11 14:31:04 -0800

    -> font var to cName declaration as ${}

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ title, subTitle }) {
+export default function Preview({ title, subTitle, font }) {
   // implement a Preview screen here
   //  Note - the HTML should have the following structure
   //  the main div should have a class of preview and the font-name prop
   //  and should have style interpolated using the alignment prop
   return (
-    <div className="preview indie-flower" style={{ textAlign: `center` }}>
+    <div className={`preview ${font}`} style={{ textAlign: `center` }}>
       <h1 key={title}>{title}</h1>
       <h3 key={subTitle}>{subTitle}</h3>
       <p>This is some blog text. </p>
