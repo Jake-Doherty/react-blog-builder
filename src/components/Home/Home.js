41b2c9c1e366b23beb5aee93c7c76225ba075193
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-11 13:11:51 -0800

    + subtitle state var & -> Pr, Ed comps

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,13 +7,14 @@ import { useState } from 'react';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState('Title');
+  const [subTitle, setSubTitle] = useState('Subtitle');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} />
-      <Editor title={title} setTitle={setTitle} />
+      <Preview title={title} subTitle={subTitle} />
+      <Editor title={title} setTitle={setTitle} subTitle={subTitle} setSubTitle={setSubTitle} />
     </main>
   );
 }
