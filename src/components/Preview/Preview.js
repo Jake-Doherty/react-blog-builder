757c09318097201aa648a84d1a56c8cdb0deee5e
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-11 15:03:07 -0800

    -> align var to style prop

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ title, subTitle, font }) {
+export default function Preview({ title, subTitle, font, align }) {
   // implement a Preview screen here
   //  Note - the HTML should have the following structure
   //  the main div should have a class of preview and the font-name prop
   //  and should have style interpolated using the alignment prop
   return (
-    <div className={`preview ${font}`} style={{ textAlign: `center` }}>
+    <div className={`preview ${font}`} style={{ textAlign: align }}>
       <h1 key={title}>{title}</h1>
       <h3 key={subTitle}>{subTitle}</h3>
       <p>This is some blog text. </p>
